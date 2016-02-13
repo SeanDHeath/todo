d32833fde7212d483007cc3bb9474657906622bc
Sean Heath <seanheath87@gmail.com>
2016-02-13 16:10:34 -0500

    initial commit

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Get the script and autocomplete
+curl https://raw.githubusercontent.com/ginatrapani/todo.txt-cli/master/todo.sh -o ~/.todo/todo.sh
+curl https://raw.githubusercontent.com/ginatrapani/todo.txt-cli/master/todo_completion -o ~/.todo/todo_completion
+chmod +x ~/.todo/todo.sh
